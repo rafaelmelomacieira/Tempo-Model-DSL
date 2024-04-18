ad09c922a7760dcafe9c15ccce1c07f672d10c4d
Rafael Melo Macieira <rfamm01@gmail.com>
2024-04-18 09:14:36 -0300

    atualização

--- src/br/ufpe/cin/greco/devc/languageStructure/Requirement.java
+++ src/br/ufpe/cin/greco/devc/languageStructure/Requirement.java
@@ -4,9 +4,14 @@ import java.util.HashSet;
 
 public abstract class Requirement {
 	
-	private HashSet<Requirement> branchs = new HashSet<Requirement>();
+	private Integer ID;
+	private Requirement origin;
 	private String action;
 	private String actionObject;
+	private String condition;
 	
 		
 }
+
+//THE SYSTEM/COMPONENT/MODULE Must Do/Load/Open the door.
+//THE SYSTEM/COMPONENT/MODULE Must Do/Load/Open the door at least 10ms.
