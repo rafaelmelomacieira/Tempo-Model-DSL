ae3e480d026ea265006f25365b5573a07241bc75
Rafael Melo Macieira <rfamm01@gmail.com>
2024-04-18 21:38:37 -0300

    Adding elements based o INCOSE requirements and needs writing.

---
+++ src/br/ufpe/cin/greco/devc/languageStructure/Needs.java
@@ -0,0 +1,5 @@
+package br.ufpe.cin.greco.devc.languageStructure;
+
+public class Needs {
+
+}
---
+++ src/br/ufpe/cin/greco/devc/languageStructure/Stakeholder.java
@@ -0,0 +1,31 @@
+package br.ufpe.cin.greco.devc.languageStructure;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Stakeholder {
+	private String name;
+	private HashMap<Version, String> analisys = new HashMap<Version, String>();
+	private HashSet<Needs> needs = new HashSet<Needs>();
+	private HashSet<Requirement> requirements = new HashSet<Requirement>();
+}
+
+/*Stakeholder "rafael macieira" {
+
+review(1){
+	
+	analisys{"sdasdasdasdsad"}
+
+	need(entity1){"adsadawfd as dfsad"}
+	need(entity2){"adsadawfd as dfsad"}
+	need(entity3){"adsadawfd as dfsad"}
+	need(entity4){"adsadawfd as dfsad"}
+	
+	req{"s sdfs fsfd sdf"}
+	req{"s sdfs fsfd sdf"}
+	req{"s sdfs fsfd sdf"}
+	req{"s sdfs fsfd sdf"}
+}
+	
+}
+*/
